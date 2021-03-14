4090388be8582d03d3a4dc83e3580d028b9bcd27
Lunush <76045303+lunush@users.noreply.github.com>
2021-03-14 20:29:59 +0300

    Update comment

--- dev/zsh/.config/zsh/aliases.zsh
+++ dev/zsh/.config/zsh/aliases.zsh
@@ -23,44 +23,44 @@ alias ll="exa -l --group-directories-first"
 alias tree="exa --tree --group-directories-first"
 
 bukusearch() {
-  rga $1 ~/.local/share/buku/bookmarks.db
+        rga $1 ~/.local/share/buku/bookmarks.db
 }
 
-# System
+# Dotfiles
 alias ez="nvim -c 'cd $HOME/.config/zsh' $HOME/.config/zsh"
 alias ev="nvim -c 'cd $HOME/.config/nvim' $HOME/.config/nvim"
 alias et="nvim -c 'cd $HOME/.config/tmux' $HOME/.config/tmux"
 
 # Random
 dudu() {
-  du -ha $1 | sort -rh | head -16
+        du -ha $1 | sort -rh | head -16
 }
 
 tokei-git() {
-  git clone --depth 1 "$1" temp-linecount-repo
-  tokei temp-linecount-repo
-  rm -rf temp-linecount-repo
+        git clone --depth 1 "$1" temp-linecount-repo
+        tokei temp-linecount-repo
+        rm -rf temp-linecount-repo
 }
 
 # Bookmarks
 alias save="buku --add"
 bulksave() {
-  cat $1 | xargs -I{} buku --add {} ${@:1}
+        cat $1 | xargs -I{} buku --add {} ${@:1}
 }
 
 # Tasks
 viewlater() {
-  link="$1"
-  title=$(wget -qO- "$link" | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si')
-  echo $title
-  description="View: $title"
-  id=$(task add +viewlater +@computer +@internet "$description" | sed -n 's/Created task \(.*\)./\1/p')
-  task "$id" annotate "$link"
+        link="$1"
+        title=$(wget -qO- "$link" | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si')
+        echo $title
+        description="View: $title"
+        id=$(task add +viewlater +@computer +@internet "$description" | sed -n 's/Created task \(.*\)./\1/p')
+        task "$id" annotate "$link"
 }
 alias vl="viewlater"
 alias inbox="task add +inbox"
 tickle() {
-  wait=$1
-  shift
-  inbox +tickle wait:$wait ${@:1}
+        wait=$1
+        shift
+        inbox +tickle wait:$wait ${@:1}
 }
