55407c02bc1ca7a62538eb27d69a7cc7c3350e86
Lunush <76045303+lunush@users.noreply.github.com>
2021-04-02 14:23:51 +0300

    Add graphql ls

--- dev/nvim/.config/nvim/lua/configs.lua
+++ dev/nvim/.config/nvim/lua/configs.lua
@@ -16,7 +16,7 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 end
 
-local servers = {'tsserver', 'vimls', 'jsonls', 'svelte', 'yamlls', 'dockerls', 'html', 'cssls', 'bashls', 'rust_analyzer', 'solargraph', 'texlab'}
+local servers = {'tsserver', 'vimls', 'jsonls', 'svelte', 'yamlls', 'dockerls', 'html', 'cssls', 'bashls',  'graphql', 'rust_analyzer', 'solargraph', 'texlab'}
 for _, server in ipairs(servers) do
   require('lspconfig')[server].setup {
     on_attach = on_attach,
