b3060ebad7f83aa76e3343644443dbf5ba0f4962
Lunush <76045303+lunush@users.noreply.github.com>
2021-04-28 20:28:19 +0300

    Clean up

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -1,5 +1,3 @@
-zinit load hlissner/zsh-autopair
-
 zinit wait lucid for \
   atinit"ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
   zdharma/fast-syntax-highlighting \
@@ -7,4 +5,5 @@ zinit wait lucid for \
   zsh-users/zsh-completions \
   zsh-users/zsh-history-substring-search \
   atload"!_zsh_autosuggest_start" \
-  zsh-users/zsh-autosuggestions
+  zsh-users/zsh-autosuggestions \
+  hlissner/zsh-autopair
