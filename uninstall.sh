4b9ab037f3e94008fe65f444081c8f461c13d001
Lunush <76045303+lunush@users.noreply.github.com>
2021-06-23 21:44:26 +0300

    Update setup scripts

--- uninstall.sh
+++ uninstall.sh
@@ -2,8 +2,3 @@
 
 # Remove symlinks
 stow -t ~ -D alacritty dotconfig nextjs-nodejs npm nvim starship wget x11 zellij zsh
-
-# Uninstall packer.nvim
-packer_home = "~/.local/share/nvim/site/pack/packer/start/packer.nvim"
-
-rm -rf $packer_home
