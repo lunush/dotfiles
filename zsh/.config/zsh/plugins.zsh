737cf9b8596157b1b79aa6e4a343c07c03d44f5c
Lunush <76045303+lunush@users.noreply.github.com>
2021-11-01 10:49:14 +0300

    fix(zsh): fix path

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -1,3 +1,3 @@
-source /usr/share/zsh/plugins/zsh-syntax-highlighting/syntax-highlighting.zsh
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
 source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
